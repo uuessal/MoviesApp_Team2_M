8b5c19d9c7d01321bb22328acc01f45aa2f9ba06
wessal <wessal.h.alharbi@outlook.com>
2025-12-25 01:30:44 +0300

    تعديلات بسيطة على ال movie center view

--- MoviesApp_Team2_M/Views/MoviesCenter.swift
+++ MoviesApp_Team2_M/Views/MoviesCenter.swift
@@ -15,68 +15,75 @@ struct MoviesCenterView: View {
         NavigationStack {
             
             ScrollView(.vertical){
-            VStack (alignment: .leading){
+            VStack (alignment: .leading ){
                 
                 
 //high rated
                 
                 Text("High rated").font(Font.title).bold()
                 ScrollView(.horizontal){
-                 HStack{
-                     
-                     
-                     ForEach(mockData.FakemoviesList , id: \.id) { Movie in
-                         ZStack (alignment: .bottomLeading) {
-                             AsyncImage(url: URL(string: Movie.poster)) { image in
-                                 image
-                                     .resizable()
-                                     .scaledToFit().cornerRadius(10).frame(width: 366, height: 434)
-                                 
-                                 
-                             } placeholder: {
-                                 
-                                 
-                             }
-                             
-                             VStack (alignment: .leading , spacing: 0.3){
-                                 
-                                 Text(Movie.name).font(Font.title.bold()).foregroundColor(.white)
-                                 
-                                 HStack (spacing: 0.1) {
-                                     
-                                     Image("filled")
-                                     Image("filled")
-                                     Image("filled")
-                                     Image("filled")
-                                     Image("unfilled")
+                    HStack (spacing : 30){
+                        
+                        
+                        ForEach(mockData.FakemoviesList , id: \.id) { Movie in
+                            ZStack (alignment: .bottomLeading) {
+                                AsyncImage(url: URL(string: Movie.poster)) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFill()                                          .frame(width: 366, height: 434)
+                                        .clipped() 
+                                        .cornerRadius(5)
+                                    
+                                    
+                                } placeholder: {
+                                    
+                                    
+                                }
+                                
+                                VStack (alignment: .leading , spacing: 0.3){
+                                    
+                                    Text(Movie.name).font(Font.title.bold()).foregroundColor(.white)
+                                    
+                                    HStack(spacing:0.4) {
+                                        ForEach(0..<4) { _ in
+                                            Image(systemName: "star.fill")
+                                                .font(.caption2)
+                                                .foregroundColor(.yellow)
+                                        }
+                                        
+                                        Image(systemName: "star")
+                                            .font(.caption2)
+                                            .foregroundColor(.yellow)
+                                    }
 
-                                 }
-                                 
-                                 HStack {
-                                     Text(String(format: "%.1f",Movie.IMDb_rating)).font(Font.title2.bold()).foregroundColor(.white)
-                                     Text("out of 5").font(Font.title3.bold()).foregroundColor(.white)
-                                 }
-                                 
-                                 HStack (spacing: 0.4){
-                                     ForEach(Movie.genre, id: \.self) { genre in
-                                         Text("\(genre)") }
-                                     Text(".\(Movie.runtime)")
-                                 }
-                                 
-
-                                 
-                             }.padding()
-                             
-                             
-                         }}
+                                    
+                                    HStack {
+                                        Text(String(format: "%.1f",Movie.IMDb_rating)).font(Font.title2.bold()).foregroundColor(.white)
+                                        Text("out of 5").foregroundColor(.white)
+                                    }
+                                    
+                                    HStack (spacing: 0.4){
+                                        ForEach(Movie.genre, id: \.self) { genre in
+                                            Text("\(genre)") }
+                                        Text(".\(Movie.runtime)")
+                                    }
+                                    
+                                    
+                                    
+                                }.padding()
+                                
+                                
+                            }}
+                        
+                        
+                        
+                        
+                        //  width: 366 , height: 434  حجمه بفيقما
+                        Rectangle().fill(Color.blue)
+                            .frame(width: 366 , height: 434)
+                    }
                     
-                     
-                     
-                     
-                            //  width: 366 , height: 434  حجمه بفيقما
-                            Rectangle().fill(Color.blue)
-                                .frame(width: 366 , height: 434)
-                        }}
+                }
                     
 //genre
                 
@@ -100,7 +107,7 @@ struct MoviesCenterView: View {
                               AsyncImage(url: URL(string: Movie.poster)) { image in
                                   image
                                       .resizable()
-                                      .scaledToFit().cornerRadius(10).frame(width: 208, height: 275)
+                                      .scaledToFit().cornerRadius(5).frame(width: 208, height: 275)
                               } placeholder: {
 
                               }
@@ -130,7 +137,7 @@ struct MoviesCenterView: View {
                                     AsyncImage(url: URL(string: Movie.poster)) { image in
                                         image
                                             .resizable()
-                                            .scaledToFit().cornerRadius(10).frame(width: 208, height: 275)
+                                            .scaledToFit().cornerRadius(5).frame(width: 208, height: 275)
                                     } placeholder: {
                                         //  ProgressView() // لودينق
                                     }
